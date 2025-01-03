bf1e5ed33883462ba646dc1dabec7f07fe6598c1
nikolanovkovic <novkovicnikola289@gmail.com>
2025-01-03 22:35:50 +0100

    komit

--- venues/routes/api.php
+++ venues/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\VenueController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,8 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/venues', [VenueController::class, 'index']);
+Route::get('/venues/page', [VenueController::class, 'indexPaginate']);
+Route::get('/venues/{id}', [VenueController::class, 'show']);
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -31,5 +35,12 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function (Request $request) {
         return auth()->user();
     });
+
+    Route::resource('/venues', VenueController::class)
+    ->only(['store', 'update', 'destroy']);
+
+    Route::resource('/reservations', ReservationController::class)
+        ->only(['store', 'update', 'destroy']);
+
 
 });
